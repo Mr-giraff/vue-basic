503c47cbdb23e7a0038f6c6e857760c9c93da3d8
Mrgiraffe <tzj_1993@163.com>
2018-08-24 14:23:16 +0800

    购物车

--- src/components/Memu.vue
+++ src/components/Memu.vue
@@ -1,24 +1,62 @@
 <template>
-    <div class="col-sm-12">
-        <table class="table">
-            <thead class="thead-default">
-            <tr>
-                <th>尺寸</th>
-                <th>价格</th>
-                <th>加入</th>
-            </tr>
-            </thead>
-            <tbody v-for="item in menuItems" :key="item.name">
-            <tr>
-                <td><strong>{{item.name}}</strong></td>
-            </tr>
-            <tr v-for="option in item.options" :key="option.size">
-                <td>{{option.size}}</td>
-                <td>{{option.price}}</td>
-                <td><button class="btn btn-sm btn-outline-success">+</button></td>
-            </tr>
-            </tbody>
-        </table>
+    <div class="row">
+        <!--菜单-->
+        <div class="col-sm-12 col-md-8">
+            <table class="table">
+                <thead class="thead-default">
+                <tr>
+                    <th>尺寸</th>
+                    <th>价格</th>
+                    <th>加入</th>
+                </tr>
+                </thead>
+                <tbody v-for="item in menuItems" :key="item.name">
+                <tr>
+                    <td><strong>{{item.name}}</strong></td>
+                </tr>
+                <tr v-for="option in item.options" :key="option.size">
+                    <td>{{option.size}}</td>
+                    <td>{{option.price}}</td>
+                    <td>
+                        <button
+                                @click="addToBasket(item,option)"
+                                class="btn btn-sm btn-outline-success">+
+                        </button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <!--购物车-->
+        <div class="col-sm-12 col-md-4">
+            <div v-if="baskets.length>0">
+                <table class="table">
+                    <thead class="thead-default">
+                    <tr>
+                        <th>数量</th>
+                        <th>品种</th>
+                        <th>价格</th>
+                    </tr>
+                    </thead>
+                    <tbody v-for="item in baskets" :key="item.size">
+                    <tr>
+                        <td>
+                            <button @click="decreaseQuantity(item)" class="btn btn-sm">-</button>
+                            <span>{{item.quantity}}</span>
+                            <button @click="increaseQuantity(item)" class="btn btn-sm">+</button>
+                        </td>
+                        <td>{{item.name}}/{{item.size}}</td>
+                        <td>{{item.price}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <p>总价：{{total + 'RMB'}}</p>
+                <button class="btn btn-success btn-block">提交</button>
+            </div>
+            <div v-else>
+                {{basketText}}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -27,6 +65,8 @@ export default {
     name: "Memu",
     data() {
         return {
+            baskets: [],
+            basketText: '购物车缺货',
             menuItems: [
                 {
                     name: '榴莲pizza',
@@ -71,6 +111,52 @@ export default {
                     ]
                 },
             ]
+        }
+    },
+    computed: {
+        total() {
+            let total = 0
+            this.baskets.forEach(item => {
+                console.log(item)
+                total += item.price
+            })
+            return total
+        }
+    },
+    methods: {
+        addToBasket(item, option) {
+            let basket = {
+                name: item.name,
+                size: option.size,
+                price: option.price,
+                quantity: 1,
+            }
+
+            if (this.baskets.length > 0) {
+                const result = this.baskets.find(basket => basket.name === item.name && basket.size === option.size)
+                if (result) {
+                    result.quantity++
+                    result.price += option.price
+                } else {
+                    this.baskets.push(basket)
+                }
+            } else {
+                this.baskets.push(basket)
+            }
+        },
+        decreaseQuantity(item) {
+            item.price -= item.price / item.quantity
+            item.quantity--
+            if (item.quantity === 0) {
+                this.removeFormBaskets(item)
+            }
+        },
+        increaseQuantity(item) {
+            item.price += item.price / item.quantity
+            item.quantity++
+        },
+        removeFormBaskets(item) {
+            this.baskets.splice(this.baskets.indexOf(item), 1)
         }
     }
 }
