4aac52feb3484745f14dff05eb8d542ba87fc5c6
Mrgiraffe <tzj_1993@163.com>
2018-08-24 10:57:21 +0800

    点餐页面

--- src/components/Memu.vue
+++ src/components/Memu.vue
@@ -1,12 +1,78 @@
 <template>
-    <h1>
-        Menu
-    </h1>
+    <div class="col-sm-12">
+        <table class="table">
+            <thead class="thead-default">
+            <tr>
+                <th>尺寸</th>
+                <th>价格</th>
+                <th>加入</th>
+            </tr>
+            </thead>
+            <tbody v-for="item in menuItems" :key="item.name">
+            <tr>
+                <td><strong>{{item.name}}</strong></td>
+            </tr>
+            <tr v-for="option in item.options" :key="option.size">
+                <td>{{option.size}}</td>
+                <td>{{option.price}}</td>
+                <td><button class="btn btn-sm btn-outline-success">+</button></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "Memu"
+    name: "Memu",
+    data() {
+        return {
+            menuItems: [
+                {
+                    name: '榴莲pizza',
+                    description: '',
+                    options: [
+                        {
+                            size: '9',
+                            price: 38,
+                        },
+                        {
+                            size: '11',
+                            price: 58,
+                        },
+                    ]
+                },
+                {
+                    name: '火龙果pizza',
+                    description: '',
+                    options: [
+                        {
+                            size: '9',
+                            price: 38,
+                        },
+                        {
+                            size: '11',
+                            price: 58,
+                        },
+                    ]
+                },
+                {
+                    name: '抹茶pizza',
+                    description: '',
+                    options: [
+                        {
+                            size: '9',
+                            price: 38,
+                        },
+                        {
+                            size: '11',
+                            price: 58,
+                        },
+                    ]
+                },
+            ]
+        }
+    }
 }
 </script>
 
